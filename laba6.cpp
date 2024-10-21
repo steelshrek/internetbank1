19dc334ceff8f3847c9acd9177749f8182cc47f3
Ivan Balokha <balyoha105@gmail.com>
2024-10-21 18:37:24 +0300

    app translate

--- laba6.cpp
+++ laba6.cpp
@@ -11,11 +11,11 @@ int main()
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	double a, b, c, x, F;
-	printf("¬вед≥ть зм≥нн≥:");
+	printf("Input variables:");
    scanf_s("%lf %lf %lf %lf",&a,&b,&c,&x);
    if (x < 0 && b != 0) { F = a * x * x; }
    else if (x > 0 && b == 0 && x != c) { F = (x - a) / (x - c); }
    else if (c > 0) { F = x / c; }
    else { printf("ERROR");};
-   printf("‘ункц≥€ дор≥внюЇ: %lf", F);
+   printf("Func equals: %lf", F);
 }
